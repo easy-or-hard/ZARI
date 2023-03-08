84520259721ed2890ace2b153780514a5a0440e6
XIYO <krb7korea@me.com>
2023-03-09 00:12:48 +0900

    파일 이동 및 변경

--- conf/ENVIROMENT_CONST.ts
+++
@@ -1,6 +0,0 @@
-const ENVIRONMENT_CONST = {
-    LOGGER_LEVEL : "debug"
-}
-Object.freeze(ENVIRONMENT_CONST);
-
-export default ENVIRONMENT_CONST;
--- conf/SERVICE_CONST.ts
+++
@@ -1,8 +0,0 @@
-const SERVICE_CONST = {
-    PORT: 80,
-    LOGGER_LEVEL: "DEBUG"
-}
-
-Object.freeze(SERVICE_CONST);
-
-export default SERVICE_CONST;
