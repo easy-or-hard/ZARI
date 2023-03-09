4780aa51cb282bc24d4a84271079ed1a7c054ef5
CHOGANGYEOL <loeygnak@gmail.com>
2023-03-09 23:11:04 +0900

    feat:confirmModal markup

--- front/src/component/modal/ConfirmModal.tsx
+++ front/src/component/modal/ConfirmModal.tsx
@@ -1,7 +1,35 @@
+import styled from "styled-components";
 
-const ConfirmModal = () => {
-    return(
-        <></>
-    )
+interface ConfirmModalProps {
+  handleClose?: () => void;
+  readonly isOpen: boolean;
 }
-export default ConfirmModal
+interface ModalProps {
+    readonly isOpen: boolean
+}
+
+const ModalWrap = styled.div<ModalProps>`
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  justify-content: center;
+  align-items: center;
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.01);
+    backdrop-filter: blur(10px);
+  }
+`;
+const ConfirmModal: React.FC<ConfirmModalProps> = ({ handleClose, isOpen }) => {
+  return (
+    <ModalWrap isOpen={isOpen}>
+      <div className="background" onClick={handleClose} />
+    </ModalWrap>
+  );
+};
+export default ConfirmModal;
