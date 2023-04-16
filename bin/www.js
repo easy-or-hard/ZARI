aa35e1cadc3e627738bf9262d951754b95c828c9
이요 <krb7korea@me.com>
2023-04-16 22:56:15 +0900

    엔트리 포인트 클래스 형식으로 수정 및 체이닝을 간단하게 수정

--- bin/www.js
+++ bin/www.js
@@ -1,20 +1,35 @@
 import app from "../app.js";
 import customProcess from "../src/server/utils/configure/custom-process.js";
 import CustomSequelize from '../src/server/utils/configure/CustomSequelize.js';
+import customLogger from '../src/server/utils/configure/custom-logger.js';
 
 // todo: remove demo data
 // import demoData from "../src/server/demo/demoData.js";
 
-new CustomSequelize()
-    .sync()
-    // .sync({force: true})
-    // .then(demoData.insertDemoData)
-    .then(() => {
-    console.log('Database is ready');
-    app.listen(customProcess.env.PORT, () => {
-        console.log(`Server is ready at ${customProcess.env.HOST}:${customProcess.env.PORT}`);
-    });
-})
-    .catch((error) => {
-        console.error(error);
-    });
+class WWW {
+    constructor() {
+        this.#init();
+    }
+
+    #init() {
+        new CustomSequelize()
+            .sync()
+            // .sync({force: true})
+            // .then(demoData.insertDemoData)
+            .then(this.#appStart)
+            .catch(this.#errorHandler);
+    }
+
+    #appStart() {
+        customLogger.info('Database is ready');
+        app.listen(customProcess.env.PORT, () => {
+            customLogger.info(`Server is ready at ${customProcess.env.HOST}:${customProcess.env.PORT}`);
+        });
+    }
+
+    #errorHandler(error) {
+        customLogger.error(error);
+    }
+}
+
+const www = new WWW();
