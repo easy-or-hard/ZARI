28eae2016ae42b58367110c6afe961224bc72475
XIYO <krb7korea@me.com>
2023-04-16 14:30:53 +0900

    주석 정리

--- src/server/MVC/controllers/zodiac-universe-controller.js
+++ src/server/MVC/controllers/zodiac-universe-controller.js
@@ -1,7 +1,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import ZodiacUniverseService from "../services/zodiac-universe-service.js";
-const { Request, Response, NextFunction } = express;
+const { Request, Response } = express;
 
 export default new class ZodiacUniverseController {
     /**
@@ -25,10 +25,9 @@ export default new class ZodiacUniverseController {
      * zodiac universe creates
      * @param {Request} req
      * @param {Response} res
-     * @param {NextFunction} next
      * @returns {Promise<void>}
      */
-    async create(req, res, next) {
+    async create(req, res) {
         const zodiacUniverse = await ZodiacUniverseService.create(req.body);
         res.status(201).json(zodiacUniverse);
     }
@@ -37,10 +36,9 @@ export default new class ZodiacUniverseController {
      * zodiac universe reads
      * @param {Request} req
      * @param {Response} res
-     * @param {NextFunction} next
      * @returns {Promise<void>}
      */
-    async findAll(req, res, next) {
+    async findAll(req, res) {
         const result = await ZodiacUniverseService.findAll();
         res.status(200).send(result);
     }
@@ -50,10 +48,9 @@ export default new class ZodiacUniverseController {
      * if not found, next route
      * @param {Request} req
      * @param {Response} res
-     * @param {NextFunction} next
      * @returns {Promise<void>}
      */
-    async findByPk(req, res, next) {
+    async findByPk(req, res) {
         const name = req.params.name;
         const result =  await ZodiacUniverseService.findByPk(name);
         res.status(200).send(result);
@@ -63,10 +60,9 @@ export default new class ZodiacUniverseController {
      * zodiac universe read all with comment
      * @param {Request} req
      * @param {Response} res
-     * @param {NextFunction} next
      * @returns {Promise<void>}
      */
-    async findAllIncludeComment(req, res, next) {
+    async findAllIncludeComment(req, res) {
         const result = await ZodiacUniverseService.findAllIncludeComment();
         res.status(200).send(result);
     }
