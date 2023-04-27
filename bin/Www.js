c437e460aada06a0c0310539a855336298977d9a
이요 <krb7korea@me.com>
2023-04-27 07:14:23 +0000

    의존성 주입으로 변경

--- bin/Www.js
+++ bin/Www.js
@@ -4,13 +4,34 @@ import CustomSequelize from '../src/server/utils/configure/CustomSequelize.js';
 import CustomLogger from '../src/server/utils/configure/CustomLogger.js';
 
 class Www {
+    /**
+     * @type {App}
+     */
     app = new App();
+
+    /**
+     * @type {CustomLogger}
+     */
     #logger = new CustomLogger();
+
+    /**
+     * @type {CustomProcess}
+     */
     #customProcess = new CustomProcess();
 
-    appStart = () => {
+    constructor(
+        app = new App(),
+        logger = new CustomLogger(),
+        customProcess = new CustomProcess()
+    ) {
+        this.app = app;
+        this.#logger = logger;
+        this.#customProcess = customProcess;
+    }
+
+    start = () => {
         new CustomSequelize()
-            .sync({force: true})
+            .sync()
             .then(this.#listen)
             .catch(this.#errorHandler);
     }
@@ -27,6 +48,4 @@ class Www {
     }
 }
 
-new Www().appStart();
-
-
+new Www().start();
